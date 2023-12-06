8cd3a47209e3b4d1db9bd43aad38440155a1e0e4
KongkongRuan <749868910@qq.com>
2023-12-06 11:07:20 +0800

    1.把神经网络放到常量里，避免重复定义

--- constant.py
+++ constant.py
@@ -1,9 +1,10 @@
 from torchvision import datasets,transforms
 import torch
+import torch.nn as nn
 showImg = False
-useGpu = False
-EPOCH = 1
-BATCH_SIZE = 200
+useGpu = True
+EPOCH = 10
+BATCH_SIZE = 20
 LR = 0.001
 DOWNLOAD = True
 device = ''
@@ -28,3 +29,45 @@ train_data = datasets.MNIST(
     transform=transforms.ToTensor(),
     download=DOWNLOAD
 )
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.layer1 = nn.Sequential(
+            # (1, 28, 28)
+            nn.Conv2d(
+                in_channels=1,  # 灰度图的通道数为1
+                out_channels=16,  # 16个filter
+                kernel_size=5,  # filter的边长为5
+                stride=1,  # 步长为1
+                padding=2,  # 因为filter的边长为5，所以padding设为(5-1)/2=2
+            ),
+            # (16, 28, 28)
+            nn.ReLU(),
+            # (16, 28, 28)
+            nn.MaxPool2d(
+                kernel_size=2,
+            ),
+            # (16, 14, 14)
+        )
+        self.layer2 = nn.Sequential(
+            # (16, 14, 14)
+            nn.Conv2d(16, 32, kernel_size=5, stride=1, padding=2),
+            # (32, 14, 14)
+            nn.ReLU(),
+            # (32, 14, 14)
+            nn.MaxPool2d(kernel_size=2)
+            # (32, 7, 7)
+        )
+        self.output_layer = nn.Linear(32 * 7 * 7, 10)
+
+    def forward(self, x):
+        # (batch, 1, 28, 28)
+        x = self.layer1(x)
+        # (batch, 16, 14, 14)
+        x = self.layer2(x)
+        # (batch, 32, 7, 7)
+        x = x.reshape(x.size(0), -1)  # 将x展开为(batch, 32*7*7)
+        # (batch, 32*7*7)
+        output = self.output_layer(x)
+        # (batch, 10)
+        return output
--- main.py
+++ main.py
@@ -14,12 +14,8 @@
 #
 # # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 import torch
-import torch.nn as nn
-from torch.autograd import Variable
 import torch.utils.data as Data
-from torchvision import datasets,transforms
 import matplotlib.pyplot as plt
-from torchsummary import summary
 import time
 
 
@@ -41,48 +37,7 @@ test_data = datasets.MNIST(
     root='./data',
     train=False
 )
-class CNN(nn.Module):
-    def __init__(self):
-        super(CNN, self).__init__()
-        self.layer1 = nn.Sequential(
-            # (1, 28, 28)
-            nn.Conv2d(
-                in_channels=1,  # 灰度图的通道数为1
-                out_channels=16,  # 16个filter
-                kernel_size=5,  # filter的边长为5
-                stride=1,  # 步长为1
-                padding=2,  # 因为filter的边长为5，所以padding设为(5-1)/2=2
-            ),
-            # (16, 28, 28)
-            nn.ReLU(),
-            # (16, 28, 28)
-            nn.MaxPool2d(
-                kernel_size=2,
-            ),
-            # (16, 14, 14)
-        )
-        self.layer2 = nn.Sequential(
-            # (16, 14, 14)
-            nn.Conv2d(16, 32, kernel_size=5, stride=1, padding=2),
-            # (32, 14, 14)
-            nn.ReLU(),
-            # (32, 14, 14)
-            nn.MaxPool2d(kernel_size=2)
-            # (32, 7, 7)
-        )
-        self.output_layer = nn.Linear(32 * 7 * 7, 10)
 
-    def forward(self, x):
-        # (batch, 1, 28, 28)
-        x = self.layer1(x)
-        # (batch, 16, 14, 14)
-        x = self.layer2(x)
-        # (batch, 32, 7, 7)
-        x = x.reshape(x.size(0), -1)  # 将x展开为(batch, 32*7*7)
-        # (batch, 32*7*7)
-        output = self.output_layer(x)
-        # (batch, 10)
-        return output
 
 
 
